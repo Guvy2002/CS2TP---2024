78f484f3c338f248568266965cc876872db3c688
Guvy2002 <gurvirbrar36@gmail.com>
2024-11-29 15:58:27 +0000

    layout

---
+++ PHP/DBconnection.php
@@ -0,0 +1,19 @@
+<?php
+function openConnect() {
+    //put in database ID's to connect to database
+    $conn = mysqli_connect(IDs);
+    return $conn;
+}
+function checkConnect ($conn) {
+    if(!$conn){
+        $connError = "Connection Error: " . mysqli_connection_error();
+        return $connError;
+    } else {
+        $connSuccess = "Connection successful";
+        return $ConnSuccess;
+    }
+}
+function closeConnect ($conn) {
+    mysqli_close($conn);
+}
+?>
