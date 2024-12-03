642a26b0ec8a328637a5000b9aaa652162157a04
Cameroony64384 <cameronmacdonald673@gmail.com>
2024-12-03 12:07:51 +0000

    Makes Checkout.php, updates signup.php and deletes signup.html so that confusion between pages stops.

---
+++ PHP/checkout.php
@@ -0,0 +1,77 @@
+<?php
+require_once('DBconnection.php');
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="CSS/HomePage.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/HomePage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+    <title>Complete your purchase</title>
+</head>
+<body>
+
+<header>
+    <h1 class="logo">Checkout</h1>
+    <ul class="menu-container">
+        <li class="menu-item">Home</li>
+        <li class="menu-item">Products</li>
+        <li class="menu-item red">For Sale</li>
+    </ul>
+    <div class="menu-icons-container">
+        <img src="Icons/Search.svg" alt="" class="icon">
+        <img src="Icons/Profile.svg" alt="" class="icon">
+        <img src="Icons/Cart.svg" alt="" class="icon">
+    </div>
+</header>
+
+<div class="progress-checkout-container">
+    <div class="progress-step-container">
+        <div class="step-check"></div>
+        <span class="step-title">Shipping</span>
+    </div>
+    <div class="progress-step-container">
+        <div class="step-check"></div>
+        <span class="step-title">Payment</span>
+    </div>
+    <div class="progress-step-container">
+        <div class="step-check"></div>
+        <span class="step-title">Review</span>
+    </div>
+</div>
+
+<div class="form-container">
+    <h2 class="form-title">Payment Details</h2>
+    <form action="" class="checkout-form">
+        <div class="input-line">
+            <label for="name">Name on card</label>
+            <input type="text" name="name" id="name" placeholder="Your name and surname">
+        </div>
+        <div class="input-line">
+            <label for="name">Card number</label>
+            <input type="text" name="name" id="name" placeholder="1111-2222-3333-4444">
+        </div>
+        <div class="input-container">
+            <div class="input-line">
+                <label for="name">Expiring Date</label>
+                <input type="text" name="name" id="name" placeholder="09-21">
+            </div>
+            <div class="input-line">
+                <label for="name">CVC</label>
+                <input type="text" name="name" id="name" placeholder="***">
+            </div>
+        </div>
+        <input type="button" value="Complete purchase">
+    </form>
+</div>
+
+</body>
+</html>
