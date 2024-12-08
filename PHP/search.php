ff7819f4f8acf94654064c8f0e498be90e800ea1
Cameroony64384 <cameronmacdonald673@gmail.com>
2024-12-08 23:34:12 +0000

    creates going from searching for and returning search bar php

---
+++ PHP/search.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'DBconnect.php';
+
+try {
+    $searchMessage = json_decode(file_get_contents("php://input"), true);
+    if (isset($searchMessage["message"])) {
+        $message = $searchMessage["message"];
+        if (strlen($message) <= 1) {
+            $products = "SELECT * FROM products WHERE fullName LIKE '$message%'";
+            $productsResult = $conn->query($products);
+        } else{
+            $products = "SELECT * FROM products WHERE fullName LIKE '%$message%'";
+            $productsResult = $conn->query($products);
+        }
+        echo json_encode([$productsResult]);
+    }
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+mysqli_close($conn);
+?>
