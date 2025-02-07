adac39035e54d45236869984efebce25bfc65fe0
Cameroony64384 <152318618+Cameroony64384@users.noreply.github.com>
2025-02-07 01:39:12 +0000

    Update test.php

--- PHP/PHPMailer-master/src/test.php
+++ PHP/PHPMailer-master/src/test.php
@@ -14,47 +14,22 @@ $mail->Host = 'smtp.gmail.com';
 $mail->Port = 465;
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 $mail->SMTPAuth = true;
+$mail->AuthType = 'XOAUTH2';
+
 $mail->Username = 'gamepointsite@gmail.com';
 $mail->Password = 'AppleCatConcrete';
+
 $mail->setFrom('gamepointsite@gmail.com', 'First Last');
-$mail->addReplyTo('cameronmacdonald673@example.com', 'First Last');
+//$mail->addReplyTo('cameronmacdonald673@example.com', 'First Last');
 $mail->addAddress('cameronmacdonald673@example.com', 'Cameron Macdonald');
 $mail->Subject = 'Testing stuff';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
 $mail->msgHTML(file_get_contents('testemail.html'), __DIR__);
-//Replace the plain text body with one created manually
 $mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
 $mail->addAttachment('a.jpg');
 
-//send the message, check for errors
 if (!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
     echo 'Message sent!';
-    if (save_mail($mail)) {
-        echo "Message saved!";
     }
 }
-
-//Section 2: IMAP
-//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
-//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
-//You can use imap_getmailboxes($imapStream, '/imap/ssl', '*' ) to get a list of available folders or labels, this can
-//be useful if you are trying to get this working on a non-Gmail IMAP server.
-/*
-function save_mail($mail)
-{
-    //You can change 'Sent Mail' to any other folder or tag
-    $path = '{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail';
-
-    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-    $imapStream = imap_open($path, $mail->Username, $mail->Password);
-
-    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    imap_close($imapStream);
-
-    return $result;
-}
-*/
